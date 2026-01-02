ae75c8afee859ebc72eb88ce6dcaad9a0ba4c145
Vijay Singh <80277854+vjsingh-piku@users.noreply.github.com>
2026-01-02 12:33:30 +0530

    Delete ReportStore class from ReportStore.php
    Remove ReportStore class and its methods.

--- src/ReportStore.php
+++ src/ReportStore.php
@@ -1,15 +1,1 @@
-<?php
-namespace WCQA;
 
-class ReportStore {
-  const OPT = 'wcqa_last_result';
-
-  public static function save_last(array $data): void {
-    update_option(self::OPT, $data, false);
-  }
-
-  public static function get_last(): ?array {
-    $d = get_option(self::OPT, null);
-    return is_array($d) ? $d : null;
-  }
-}
